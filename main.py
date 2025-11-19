d0664038b0482aacc0db3dafb39f448b41d2c847
antonioroserom <wrosero9059@utm.edu.ec>
2025-11-18 22:04:57 -0500

    Agrego bloque de prueba para el sistema de turnos médicos, el cual, añade pacientes, atiende al primero y muestra el estado de la cola

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from src.soluciones import *
+
+# 1: Turnos médicos
+print("=== 1. Sistema de turnos médicos ===")
+cola_pacientes = []
+agregar_paciente(cola_pacientes, "Carlos Rubira", 34, "Cardiología")
+agregar_paciente(cola_pacientes, "Maite Gully", 27, "Ginecología")
+print("Pacientes en espera:", mostrar_pacientes(cola_pacientes))
+atendido = atender_paciente(cola_pacientes)
+print("Paciente atendido:", atendido)
+print("Pacientes restantes:", mostrar_pacientes(cola_pacientes))
+
+print("\n" + "="*50 + "\n")
